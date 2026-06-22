f4f283a5c601503aa56def1d1e9a318dfc08b733
Kevin Carlson <kcarlson@squidsoftwaresolutions.com>
2026-06-22 14:25:17 -0700

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.java
@@ -0,0 +1,24 @@
+class KthLargest {
+
+    private Queue<Integer> minHeap;
+    private int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        this.minHeap = new PriorityQueue<>();
+        for (int i = 0; i < nums.length; i++) {
+            minHeap.add(nums[i]);
+            if (minHeap.size() > k) {
+                minHeap.remove();
+            };
+        }
+    }
+    
+    public int add(int val) {
+        minHeap.add(val);
+        if (minHeap.size() > k) {
+            minHeap.poll();
+        }
+        return minHeap.peek();
+    }
+}
